fe3fa866ead17112e9ae6c383f4ef651de9acf8c
Rudnev Viktor <rudnevvictor2003@mail.ru>
2023-10-24 06:22:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/camerainapp/ViewActivity.kt
@@ -0,0 +1,46 @@
+package com.example.camerainapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.widget.LinearLayout
+import androidx.annotation.UiThread
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.camerainapp.databinding.ActivityViewBinding
+import java.io.File
+
+class ViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var binding = ActivityViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var recyclerView = binding.rv
+        val directory = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "date")
+        var dataAdapter = DateAdapter(directory)
+        recyclerView.adapter = dataAdapter
+
+        val lm = LinearLayoutManager(this)
+        lm.orientation = LinearLayoutManager.VERTICAL
+
+        recyclerView.layoutManager = lm
+
+        Thread {
+            Thread.sleep(1000)
+            runOnUiThread {
+                val adapter = DateAdapter(directory)
+                recyclerView.adapter = adapter
+                adapter.notifyDataSetChanged()
+            }
+        }.start()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        startActivity(Intent(this, CameraActivity::class.java))
+        finish()
+    }
+}
